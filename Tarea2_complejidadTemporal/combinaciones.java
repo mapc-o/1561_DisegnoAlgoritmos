0165708ebb27f00b721ccde5a325c1841f4f152e
mapc-o <151883869+mapc-o@users.noreply.github.com>
2024-08-15 20:53:41 -0600

    Update combinaciones.java

--- Tarea2_complejidadTemporal/combinaciones.java
+++ Tarea2_complejidadTemporal/combinaciones.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
 
+public class combinaciones {
+
+    static Scanner scan = new Scanner(System.in);   //Asignación                  T(n) = 1
+
+
+    public static void main(String[] args){
+        int[] nums = new int[3];    //Asignacion                                  T(n) = 1
+        int count = 0;  //Asignacion                                              T(n) = 1
+        
+        for(int i = 0; i < nums.length; i++){
+            System.out.println("Introduce un número: ");
+            nums[i] = scan.nextInt();   //Bucle dependiente de variable           T(n) = n
+        }
+
+
+        for(int i = 0; i < nums.length; i++){                   
+            for(int j = 0; j < nums.length; j++){   // Bucle anidado              T(n) = n^2
+                System.out.println("( "+nums[i]+ " , "+nums[j]+" )");
+                count++;
+            }
+        }
+
+        System.out.println("Salida: " + count);                     //Impresión   T(n) = 1
+
+                                            //COSTE TOTAL                         T(n) = n^2 + n + 4
+
+
+    }
+
+}
