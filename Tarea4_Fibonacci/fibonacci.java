c2b4e17c0ab81dfc505709da7450c94384faac2d
mapc-o <151883869+mapc-o@users.noreply.github.com>
2024-09-02 21:16:24 -0600

    Update fibonacci.java

--- Tarea4_Fibonacci/fibonacci.java
+++ Tarea4_Fibonacci/fibonacci.java
@@ -1,1 +1,24 @@
+public class fibo{
+    public static int fibo(int n){
+        if(n == 0){
+            return 0;
+    
+        } else if(n == 1){
+            return 1;
+    
+        } else{
+            return fibo(n-1) + fibo(n-2);
+    
+        }
+    }
+
+
+    
+    public static void main(String[] args) {
+        for(int i=0; i < 20; i++){
+            System.out.println(fibo(i));
+        }
+   
+    }
+}
 
