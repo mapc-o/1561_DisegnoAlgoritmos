88473a3ca93ace4efef802649af79fb598204199
mapc-o <151883869+mapc-o@users.noreply.github.com>
2024-09-17 18:47:48 -0600

    Update busquedaLinealCentinela.java

--- Tarea5/busquedaLinealCentinela.java
+++ Tarea5/busquedaLinealCentinela.java
@@ -1,23 +1,27 @@
 public class busquedaLinealCentinela {
 	public static int busqueda_lineal_centinela(int[] data, int a_buscar) {
-		int n = data.length;
-		int i = 0;
-        int tmp = data[n-1];
-        data[n-1] = a_buscar;
+		int n = data.length; //T(n)= 1
+		int i = 0; //T(n)= 1
+        int tmp = data[n-1]; //T(n)= 1
+        data[n-1] = a_buscar; //T(n)= 1
 		while (data[i] != a_buscar) {
-			i = i +1;
+			i = i +1; //T(n)= n
 		}
 
-        data[n-1] = tmp;
+        data[n-1] = tmp; //T(n)= 1
 
 		if (i < n-1 || a_buscar == data[n-1]) {
 			return i;
 
 		} else {
-			return -1;
+			return -1; //T(n)= 1
 		}
 
 	}
+
+	//T(n)= 1+1+1+1+n+1+1
+	//T(n)= n+6
+	//O(n)
 
 
 	public static void main(String[]args){
