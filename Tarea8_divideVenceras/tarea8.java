90a722603ca7d8befc5b80147e00fb90ae47000c
mapc-o <151883869+mapc-o@users.noreply.github.com>
2024-10-22 09:10:01 -0600

    Update tarea8.java

--- Tarea8_divideVenceras/tarea8.java
+++ Tarea8_divideVenceras/tarea8.java
@@ -1,26 +1,30 @@
 public class tarea8 {
 
-	public static int escalones(int n){
-		int[]memoria = new int[n+1];
-		memoria[0] = 0;
-		memoria[1] = 1;
-		memoria[2] = 2;
-		if(n == 0 || n == 1 || n == 2){
-			return n;
+	public static int escalones(int n) {
+		int[] memoria = new int[n + 1]; // T(n) = 1
+		memoria[0] = 0; // T(n) = 1
+		memoria[1] = 1; // T(n) = 1
+		memoria[2] = 2; // T(n) = 1
+		if (n == 0 || n == 1 || n == 2) {
+			return n; // T(n) = 1
 		}
 
-		for (int i = 3; i <= n; i++){
-			memoria[i] = memoria[i-1] + memoria[i-2] + memoria[i-3];
-			System.out.println(memoria[i]);
+		for (int i = 3; i <= n; i++) {
+			memoria[i] = memoria[i - 1] + memoria[i - 2] + memoria[i - 3];
+			System.out.println(memoria[i]); // T(n) = 2n
 
 		}
-		return memoria[n];
+		return memoria[n]; // T(n) = 1
 
+		//T(n) = 1+1+1+1+1+2n+1
+		//T(n) = 2n + 6
+		//T(n) = 2n
+		//O(n)
 
 	}
 
 	public static void main(String[] args) {
-		int n = 8;
-		System.out.println("Formas de subir "+n +" escalones: " +escalones(n));
+		int n = 8;	//O(1)
+		System.out.println("Formas de subir " + n + " escalones: " + escalones(n));	//O(n)
 	}
 }
